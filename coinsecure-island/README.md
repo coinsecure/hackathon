20b6b5077e28266f815d65e06c7b19472d0a5fc4
vivek0 <vivek0@users.noreply.github.com>
2015-07-21 18:09:17 +0530

    Added coinsecure-island
    By @basavk

---
+++ coinsecure-island/README.md
@@ -0,0 +1,8 @@
+# coinsecure-island
+A fun way to learn about bitcoin while exploring a beautiful island. It also provides live trades(ask/bids) from CoinSecure Trading Platform for every 2 mins. Explore this island to know more about 'Bitcoin'.
+
+How to run ?
+Extract the zip file which is present under executable folder & run the exe file.
+
+Demo Video:
+https://www.youtube.com/watch?v=kKhlpmUn7fI
---
+++ coinsecure-island/src/Assets/webrequest.js
@@ -0,0 +1,62 @@
+﻿#pragma strict
+
+import SimpleJSON;
+
+public var url: String = "https://api.coinsecure.in/v0/noauth/ticker";
+
+function Start () {
+	Debug.Log("Initializing..");
+	// Every 2 mins
+	InvokeRepeating("GetDataOnInterval", 0, 120);
+}
+
+
+function Update () {
+
+}
+
+function GetDataOnInterval() {
+	GetDataFromServer();
+}
+
+function GetDataFromServer() {
+
+  Debug.Log("Invoked..");
+  var www : WWW = new WWW (url);
+
+  yield www;
+
+  if (www.error == null) { 
+   Debug.Log("WWW OK: " + www.text);
+   
+   var mydata = JSON.Parse(www.text);
+   
+   Debug.Log("Server data" + mydata);
+   
+   var highestBid = parseInt(mydata["result"][4]["highestBid"]) / 100;
+   var lowestAsk  = parseInt(mydata["result"][5]["lowestAsk"]) / 100;
+   
+   
+   var finalString = "Highest Bid : " + highestBid + " Rs \nLowest Ask : " + lowestAsk + " Rs";
+   
+   Debug.Log(finalString);   
+   
+   var playerObject : GameObject = GameObject.FindWithTag("Player");   	
+   	   	
+    
+    var textMesh: TextMesh = new TextMesh();
+    textMesh = playerObject.GetComponent(TextMesh);
+    
+    textMesh.text = finalString;
+  
+   Debug.Log(finalString);
+   
+  
+   
+  } else { 
+   Debug.Log("WWW Error: "+ www.error); 
+  } 
+}
+
+
+
